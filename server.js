2395de44219ab8e3dd5b6afd316027203a2a9be6
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-12-10 18:49:38 -0800

    allow user to create a new database entry

--- server.js
+++ server.js
@@ -3,16 +3,35 @@ const express = require('express');
 const app = express();
 const pg = require('pg');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT;
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/v1/cards', (req, res) => {
     client.query(`SELECT * FROM cards;`)
-        .then(data => res.send(data.rows));
+        .then(data => res.send(data.rows))
+        .catch(console.error);
+});
+
+app.post('/api/v1/cards', (req, res) => {
+    console.log('---------------------------------------');
+    console.log(req.body);
+    client.query(`
+        INSERT INTO cards (recipient, sender, content)
+        VALUES ($1, $2, $3);
+        `, [
+            req.body.recipient,
+            req.body.sender,
+            req.body.content
+        ])
+        .then(data => res.send(data.rows))
+        .catch(console.error);
 });
 
 app.get('/api/v1/cards/:id', (req, res) => {
@@ -25,14 +44,10 @@ app.get('/api/v1/cards/:recipient', (req, res) => {
     client.query(
         `SELECT * FROM cards WHERE recipient = $1;`,
         [   
-            req.params.recipient,
-            req.params.recipient,
-            req.params.recipient,
-            req.params.recipient,
-            req.params.recipient, 
-            req.params.recipient,
+            req.params.recipient
         ])
-        .then(data => res.send(data.rows));
+        .then(data => res.send(data.rows))
+        .catch(console.error);
 });
 
 app.listen(PORT, () => {
