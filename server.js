26ce2c30ecfb7e65d6e3dbcdb575f05f3518f34c
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-12-11 07:07:21 -0800

    add PUT and DELETE routes for single card

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post('/api/v1/cards', (req, res) => {
             req.body.sender,
             req.body.content
         ])
-        .then(data => res.send(data.rows))
+        .then(data => res.status(201).send(data.rows))
         .catch(console.error);
 });
 
@@ -39,6 +39,29 @@ app.get('/api/v1/cards/:id', (req, res) => {
         .then(data => res.send(data.rows))
         .catch(console.error);
 });
+
+app.put('/api/v1/cards/:id', (req, res) => {
+    client.query(`
+        UPDATE cards SET recipient=$1, sender=$2, content=$3
+        WHERE id=$4;
+        `, [
+            req.body.recipient,
+            req.body.sender,
+            req.body.content,
+            req.params.id
+        ])
+        .then(data => res.status(200).send('Card updated'))
+        .catch(console.error);
+});
+
+app.delete('/api/v1/cards/:id', (req, res) => {
+    client.query(`
+        DELETE FROM cards WHERE id = $1;
+        `, [req.params.id])
+        .then(data => res.send('Data deleted.').status(204))
+        .catch(console.error);
+});
+
 
 app.get('/api/v1/cards/:recipient', (req, res) => {
     client.query(
